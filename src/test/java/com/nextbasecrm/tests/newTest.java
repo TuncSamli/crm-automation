5cb1f0d43538a55d3a2e1867eb60a9736852c5ce
Linderwow <alexyiyi92@gmail.com>
2022-02-26 13:24:06 -0500

    Initial Commit

--- src/test/java/com/nextbasecrm/tests/newTest.java
+++ src/test/java/com/nextbasecrm/tests/newTest.java
@@ -1,4 +1,39 @@
 package com.nextbasecrm.tests;
 
-public class newTest {//
+import com.nextbasecrm.utilities.CRM_Utilities;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class newTest {
+    WebDriver driver;
+@BeforeMethod
+    public void  setUpMethod(){
+        driver=WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+}
+    @Test
+    public void hr85Login()throws InterruptedException{
+    CRM_Utilities.crm_login(driver,"hr85@cydeo.com","UserUser");
+    CRM_Utilities.crm_proFileClick(driver);
+}
+    @Test
+    public void helpdesk85Login(){
+    CRM_Utilities.crm_login(driver,"helpdesk85@cydeo.com","UserUser");
+    CRM_Utilities.crm_proFileClick(driver);
+}
+    @Test
+    public void marketing85Login() {
+    CRM_Utilities.crm_login(driver,"marketing85@cydeo.com","UserUser");
+    CRM_Utilities.crm_proFileClick(driver);
+}
+@AfterMethod
+    public void afterMethod(){
+    driver.close();
+}
 }
--- src/test/java/com/nextbasecrm/utilities/CRM_Utilities.java
+++ src/test/java/com/nextbasecrm/utilities/CRM_Utilities.java
@@ -3,16 +3,22 @@ package com.nextbasecrm.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class CRM_Utilities {
     /*
     This method will log in with helpdesk1@cybertekschool.com
      user when it is called
      */
-    public static void crm_login(WebDriver driver){
+    public static void crm_login(WebDriver driver) {
+        crm_getToWebsite(driver);
         //3. Enter valid username
         WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
-        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
+        inputUsername.sendKeys("helpdesk85@cydeo.com");
 
         //helpdesk1@cybertekschool.com  UserUser
         //Helpdesk2@cybertekschool.com  UserUser
@@ -26,7 +32,8 @@ public class CRM_Utilities {
         loginButton.click();
     }
 
-    public static void crm_login(WebDriver driver, String username, String password){
+    public static void crm_login(WebDriver driver, String username, String password) {
+        crm_getToWebsite(driver);
         //3. Enter valid username
         WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
         inputUsername.sendKeys(username);
@@ -41,6 +48,20 @@ public class CRM_Utilities {
         //5. Click to Log In button
         WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
         loginButton.click();
+
+    }
+        public static void crm_getToWebsite (WebDriver driver) {
+
+            driver.get("https://login1.nextbasecrm.com/");
+        }
+        public static void crm_proFileClick(WebDriver driver){
+        WebElement profileMenuLocator = driver.findElement(By.xpath("//div[@onclick='showUserMenu()']"));
+        profileMenuLocator.click();
     }
 
-}
+
+
+
+        }
+
+
