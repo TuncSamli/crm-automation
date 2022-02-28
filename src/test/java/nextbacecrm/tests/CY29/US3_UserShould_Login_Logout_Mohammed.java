6eaa28ebceabbbd9694ee242b9fc97748ba4397b
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-28 17:04:46 -0500

    commit1 after tunc correct

---
+++ src/test/java/nextbacecrm/tests/CY29/US3_UserShould_Login_Logout_Mohammed.java
@@ -0,0 +1,9 @@
+package nextbacecrm.tests.CY29;
+
+public class US3_UserShould_Login_Logout_Mohammed {
+
+//123
+    }
+
+
+
--- src/test/java/nextbacecrm/tests/US3_UserSholdLogout_Mohammed.java
+++
@@ -1,5 +0,0 @@
-package test.java.nextbacecrm.tests;
-
-public class US3_UserSholdLogout_Mohammed {
-    //123
-}
