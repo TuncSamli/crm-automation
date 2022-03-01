7435266ab7cdcffe6b7859f45cdfd0a198d7638a
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-28 20:14:18 -0500

    Mohammed commit as a trail

--- src/test/java/nextbacecrm/tests/CY29/US3_UserShould_Login_Logout_Mohammed.java
+++ src/test/java/nextbacecrm/tests/CY29/US3_UserShould_Login_Logout_Mohammed.java
@@ -1,8 +1,150 @@
 package nextbacecrm.tests.CY29;
+import nextbacecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 public class US3_UserShould_Login_Logout_Mohammed {
 
-//123
+   public WebDriver driver;
+String url ="https://login2.nextbasecrm.com/";
+
+
+
+   @BeforeMethod
+    public void setUpMethod (){
+    driver = WebDriverFactory.getDriver("chrome");
+   driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get(url);
+   }
+
+
+
+
+   @Test
+    public void login1 (){
+       WebElement userName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+userName.sendKeys("hr85@cydeo.com");
+
+WebElement passWord = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+passWord.sendKeys("UserUser");
+
+WebElement LogInButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+LogInButton.click();
+
+WebElement userBlock =driver.findElement(By.xpath("//div[@id='user-block']"));
+
+userBlock.click();
+
+WebElement LogOutButton =driver.findElement(By.xpath("//span[.='Log out']"));
+LogOutButton.click();
+
+
+//Assert.assertEquals(driver.getCurrentUrl(),url);
+
+
+
+   }
+
+
+@Test
+
+
+public void login2 (){
+
+    WebElement userName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+    userName.sendKeys("helpdesk1@cydeo.com");
+
+    WebElement passWord = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+    passWord.sendKeys("UserUser");
+
+    WebElement LogInButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+    LogInButton.click();
+
+    WebElement userBlock =driver.findElement(By.xpath("//div[@id='user-block']"));
+
+    userBlock.click();
+
+    WebElement LogOutButton =driver.findElement(By.xpath("//span[.='Log out']"));
+    LogOutButton.click();
+
+
+
+}
+
+@Test
+
+public void login3 (){
+
+    WebElement userName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+    userName.sendKeys("marketing1@cydeo.com");
+
+    WebElement passWord = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+    passWord.sendKeys("UserUser");
+
+    WebElement LogInButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+    LogInButton.click();
+
+    WebElement userBlock =driver.findElement(By.xpath("//div[@id='user-block']"));
+
+    userBlock.click();
+
+    WebElement LogOutButton =driver.findElement(By.xpath("//span[.='Log out']"));
+    LogOutButton.click();
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
