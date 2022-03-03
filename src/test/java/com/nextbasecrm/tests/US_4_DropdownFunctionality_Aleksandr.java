8b9c6029c8b69bcb7790f52c8ac754adba91ed6f
Linderwow <alexyiyi92@gmail.com>
2022-03-02 19:20:36 -0500

    Initial Commit

--- src/test/java/com/nextbasecrm/tests/US_4_DropdownFunctionality_Aleksandr.java
+++ src/test/java/com/nextbasecrm/tests/US_4_DropdownFunctionality_Aleksandr.java
@@ -2,11 +2,15 @@ package com.nextbasecrm.tests;
 
 import com.nextbasecrm.utilities.CRM_Utilities;
 import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class US_4_DropdownFunctionality_Aleksandr {
@@ -18,22 +22,42 @@ public class US_4_DropdownFunctionality_Aleksandr {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 }
     @Test
-    public void hr85Login()throws InterruptedException{
+    public void hr85Login(){
     CRM_Utilities.crm_login(driver,"hr85@cydeo.com","UserUser");
     CRM_Utilities.crm_proFileClick(driver);
+        WebElement find = driver.findElement(By.xpath("//span[@class='user-name']"));
+        Assert.assertTrue(find.isDisplayed());
+        List<WebElement>all=driver.findElements(By.xpath("//div[@id='menu-popup-user-menu']"));
+        for(WebElement each:all){
+            System.out.println(each.getText());
+        }
 }
     @Test
-    public void helpdesk85Login(){
-    CRM_Utilities.crm_login(driver,"helpdesk85@cydeo.com","UserUser");
-    CRM_Utilities.crm_proFileClick(driver);
-}
-    @Test
-    public void marketing85Login() {
-    CRM_Utilities.crm_login(driver,"marketing85@cydeo.com","UserUser");
-    CRM_Utilities.crm_proFileClick(driver);
-}
-@AfterMethod
-    public void afterMethod(){
-    driver.close();
-}
-}
+    public void helpdesk85Login() {
+        CRM_Utilities.crm_login(driver, "helpdesk85@cydeo.com", "UserUser");
+        CRM_Utilities.crm_proFileClick(driver);
+        WebElement find = driver.findElement(By.xpath("//span[@class='user-name']"));
+        Assert.assertTrue(find.isDisplayed());
+        List<WebElement> all = driver.findElements(By.xpath("//div[@id='menu-popup-user-menu']"));
+        for (WebElement each : all) {
+            System.out.println(each.getText());
+        }
+        }
+
+        @Test
+        public void marketing85Login () {
+            CRM_Utilities.crm_login(driver, "marketing85@cydeo.com", "UserUser");
+            CRM_Utilities.crm_proFileClick(driver);
+            WebElement find = driver.findElement(By.xpath("//span[@class='user-name']"));
+            Assert.assertTrue(find.isDisplayed());
+            List<WebElement> all = driver.findElements(By.xpath("//div[@id='menu-popup-user-menu']"));
+            for (WebElement each : all) {
+                System.out.println(each.getText());
+            }
+        }
+            @AfterMethod
+            public void afterMethod () {
+                driver.close();
+            }
+        }
+
